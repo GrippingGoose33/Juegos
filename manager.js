e4cb727777c06ffb537235d5bedacb62f91bda29
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-06-24 10:17:34 -0500

    GYN

--- manager.js
+++ manager.js
@@ -1,7 +1,8 @@
 const manager = [
-    {id: 'GMN', description: "Adivina el numero de la maquina y ve si la maquina adivina tu numero", title:"Guess my number", img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-8d6_HblcFpm48MJqy8VdQoXJP4k8ObUJmw&usqp=CAU'},
-    {id: 'PPt', description: "Escoge tu objeto y ve si ganas o pierdes", title:"Piedra, Papel o Tijera", img: 'https://png.pngtree.com/element_our/20200702/ourlarge/pngtree-rock-paper-scissors-cloth-guess-fist-gesture-image_2287305.jpg'},
-    {id: '', description: "Proximamente", title:"Proximamente", img: 'https://www.dcs.mx/Manual%20de%20usuario/tutoriales/images/5.jpg'},
+    {id: 'GMN', description: "Adivina el numero de la maquina", title:"Guess my number", img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-8d6_HblcFpm48MJqy8VdQoXJP4k8ObUJmw&usqp=CAU'},
+    {id: 'GYN', description: "Ve si la maquina adivina tu numero", title:"Guess your number", img: 'https://png.pngtree.com/element_our/20200702/ourlarge/pngtree-rock-paper-scissors-cloth-guess-fist-gesture-image_2287305.jpg'},
+    {id: 'PPT', description: "Proximamente", title:"Piedra, papel o tijera", img: 'https://png.pngtree.com/element_our/20200702/ourlarge/pngtree-rock-paper-scissors-cloth-guess-fist-gesture-image_2287305.jpg'},
+    {id: 'Proximo', description: "Proximamente", title:"Proximamente", img: 'https://www.dcs.mx/Manual%20de%20usuario/tutoriales/images/5.jpg'},
 ]
 
 export default manager;
