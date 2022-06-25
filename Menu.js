2b0579529d3859adbc89f98b0091c306cf73904c
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-06-25 13:38:10 -0500

    Box

--- Menu.js
+++ Menu.js
@@ -2,10 +2,7 @@ import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import './styles.js';
 import Box from './Box';
-import GMN from './GMN';
-import GYN from './GYN';
-import PPT from './PPT';    
-import manager from'./manager';
+import manager from './manager';
 
 
 function Store({navigation}) {
@@ -14,45 +11,18 @@ function Store({navigation}) {
         <View style = {styles.root}>
             <ScrollView>
             <View style = {styles.containerBox}>
-                <View style={styles.root2}>
-                <Text style={styles.text}>{manager.title}</Text>
-                <Image
-                    style={styles.tinyLogo}
-                    source={{uri:img}}
+                {
+                manager.map((manager, v)=> (
+                    <Box key={v}
+                    img = {manager.img}
+                    id = {manager.id}
+                    description = {manager.description}
+                    stock = {manager.stock}
+                    title = {manager.title}
                 />
-                <Text>{description}</Text>
-                <Button
-                    color="#455q64"
-                    title={title}
-                    onPress={() => navigation.navigate( GMN )}
-                />
-                </View>
-                <View style={styles.root2}>
-                <Text style={styles.text}>{manager.title}</Text>
-                <Image
-                    style={styles.tinyLogo}
-                    source={{uri:img}}
-                />
-                <Text>{description}</Text>
-                <Button
-                    color="#455q64"
-                    title={title}
-                    onPress={() => navigation.navigate( GYN )}
-                />
-                </View>
-                <View style={styles.root2}>
-                <Text style={styles.text}>{manager.title}</Text>
-                <Image
-                    style={styles.tinyLogo}
-                    source={{uri:img}}
-                />
-                <Text>{description}</Text>
-                <Button
-                    color="#455q64"
-                    title={title}
-                    onPress={() => navigation.navigate( PPT )}
-                />
-                </View>
+                
+                ))
+                }
             </View>
             </ScrollView>
         </View>
@@ -74,27 +44,6 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
         alignItems: 'center',
     },
-
-    root2: {
-        width: 150,
-        height: 150,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#8BF50C",
-        borderWidth: 1,
-        borderRadius: 0,
-        margin: 10,
-    },
-
-    tinyLogo: {
-        width: 80,
-        height: 80,
-    },
-
-    text: {
-        flex: 1,
-        alignItems: "center"
-    }
 
 })
 
